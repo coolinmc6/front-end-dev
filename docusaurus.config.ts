b216b26bf8d8e8b69daf8eca0ae0f733aa2bb98b
Colin McNamara <coolinmc6@gmail.com>
2024-11-26 16:45:21 -0500

    Add deploy actions for github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://coolinmc6.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/front-end-dev/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'coolinmc6.github.io', // Usually your repo name.
+  organizationName: 'coolinmc6', // Usually your GitHub org/user name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
